49793ae47b9ee0c4060f2ec912542d5230de842f
ahemenson <ahemenson@gmail.com>
2019-03-26 08:46:18 -0300

    implementing other examples

--- hellocucumber/src/test/java/hellocucumber/Stepdefs.java
+++ hellocucumber/src/test/java/hellocucumber/Stepdefs.java
@@ -6,23 +6,38 @@ import cucumber.api.java.en.Then;
 import static org.junit.Assert.*;
 
 class IsItFriday {
-	static String isItFriday(String today) {
-		return null;
+    static String isItFriday(String today) {
+	if (today.equals("Friday")) {
+	  return "TGIF";
 	}
+    return "Nope";
+  }
 }
 
 public class Stepdefs {
 	private String today;
 	private String actualAnswer;
 
-	@Given("^today is Sunday$")
-	public void today_is_Sunday() {
-		today = "Sunday";
-	}
+//	@Given("^today is Sunday$")
+//	public void today_is_Sunday() {
+//		today = "Sunday";
+//	}
+//
+//	@Given("^today is Friday$")
+//	public void today_is_Friday(){
+//		this.today = "Friday";
+//		
+//	}
+	
+	@Given("^today is \"([^\"]*)\"$")
+    public void today_is(String today) {
+		this.today = today;
+    }
+	
 
 	@When("^I ask whether it's Friday yet$")
 	public void i_ask_whether_it_s_Friday_yet() {
-		actualAnswer = IsItFriday.isItFriday(today);
+		this.actualAnswer = IsItFriday.isItFriday(today);
 	}
 
 	@Then("^I should be told \"([^\"]*)\"$")
